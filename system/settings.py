c28a39e24cd4b68ade1ddcdbd47c07e406586e50
Hane601 <alstudiesonly@gmail.com>
2025-12-31 15:42:35 +0530

    Update settings.py

--- system/settings.py
+++ system/settings.py
@@ -114,11 +114,10 @@ USE_TZ = True
 # --------------------------------------------------
 # STATIC & MEDIA FILES
 # --------------------------------------------------
-STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
